7bec0526180167bc7576d307331099575ffef314
John Doherty <johndoherty1984@live.com>
2024-05-04 12:17:08 +0000

    Admin panel power-up

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-jdohertydev-djangoblog-y2oqsr15j9k.ws-eu111.gitpod.io", ".herokuapp.com"]
 
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_summernote', 
     'blog',
+    
 ]
 
 MIDDLEWARE = [
